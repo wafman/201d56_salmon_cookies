d7dd9c72553e4ffb0187bcd4b495b959103cf540
Will Fritts <waf515@gmail.com>
2019-04-01 14:26:51 -0700

    set initial logic

---
+++ js/app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+//global variable for all store hours
+var hours = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 ];
+
+//initialize first store and then test funcitonality before expanding
+var Pike = {
+  //starting data
+  minimum : [],
+  maximum : [],
+  average : [],
+  customers : [],
+  cookies: [],
+  // total : ,
+
+  // methods below here
+
+  //this method generates random customer numbers for each our of the day
+  generateCustomers : function cumstomerGenerator(){
+    for(var i = 0; i < hours.length; i++){
+      Math.floor(Math.random() * 66);
+      console.log(i);
+      customers.push(i);
+    }
+  },
+};
